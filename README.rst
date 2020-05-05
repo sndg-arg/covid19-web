ef14e13d4ac45d6eec22fc6831af44c06d80bf5c
Ezequiel Sosa <ezequieljsosa@gmail.com>
2020-05-05 07:45:32 +0100

    initial commit

---
+++ README.rst
@@ -0,0 +1,7 @@
+SNDG-COVID19
+============
+
+SNDG web to show argentinian COVID19 isolates
+
+
+docker run --rm -v $PWD:/out -w /out node:lts npm run-script build
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+
+module.exports = {
+    entry: './sndg_covid19/static/js/project.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'staticfiles'),
+    },
+    node: {
+        fs: 'empty'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader']
+            }, {
+                test: /\.(ttf|eot|svg|gif|png)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                use: [{
+                    loader: 'file-loader'
+                }]
+            }
+        ],
+    },
+    resolve: {
+        alias: {
+            'handlebars': 'handlebars/dist/handlebars.js'
+            // Libraries msa and seqview are old and use require on handlebars that throws a warning
+        }
+    },
+    mode:'development',
+    stats: {
+        warningsFilter: [
+            'filter',
+            /filter/,
+            (warning) => warning.indexOf('msa/lib/') != -1
+            // Library msa is old so it throws many unfixable warnings
+        ]
+    }
+};
