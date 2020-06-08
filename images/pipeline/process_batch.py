9fb03dc6301dc80e697688f13b2eeac5a4a93211
Ezequiel Sosa <ezequieljsosa@gmail.com>
2020-06-08 14:08:10 -0300

    pipeline update

--- images/pipeline/process_batch.py
+++ images/pipeline/process_batch.py
@@ -6,9 +6,10 @@ from collections import defaultdict
 from tqdm import tqdm
 
 import argparse
+
 parser = argparse.ArgumentParser(description='')
-parser.add_argument("-i",'--dir_entrada', type=str, help='Directorio con los fastq, un solo par por muestra')
-parser.add_argument("-o",'--dir_salida', type=str, help='Directorio donde se pone las salidas por muestra ')
+parser.add_argument("-i", '--dir_entrada', type=str, help='Directorio con los fastq, un solo par por muestra')
+parser.add_argument("-o", '--dir_salida', type=str, help='Directorio donde se pone las salidas por muestra ')
 args = parser.parse_args()
 
 assert os.path.exists(args.dir_entrada), f"{args.dir_entrada} no existe"
@@ -20,12 +21,13 @@ for arch in glob(f"{args.dir_entrada}/*.fastq.gz"):
     muestra = arch.split("/")[-1][:9]
     muestras[muestra].append(arch)
 print(f"se encontraron {len(muestras)} en {args.dir_entrada}")
-for k,v in muestras.items():
+for k, v in muestras.items():
     assert len(v) == 2, f"error: {k} tiene {len(v)} archivos"
 
 with tqdm(muestras.items()) as pbar:
-    for k,v in pbar:
+    for k, v in pbar:
         pbar.set_description(f"procesando {k}")
-
-        cmd = f'process_sample.sh {v[0]} {v[1]} {k} {args.dir_salida}'
-        sp.call(cmd,shell=True)
+        if not os.path.exists(args.dir_salida + "/" + k):
+            os.makedirs(args.dir_salida + "/" + k)
+        cmd = f'process_sample.sh {v[0]} {v[1]} {k} {args.dir_salida} > {args.dir_salida}/{k}/log.txt   2>&1'
+        sp.call(cmd, shell=True)
